d2cde82a7d0a44d4b3ab2ad6579de4df960ca93e
Thipak <thipak01balaji@gmail.com>
2025-07-02 16:57:04 +0530

    Experience Item creation

--- src/views/experience/Experience.jsx
+++ src/views/experience/Experience.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/header/header";
+import { Item } from "./item";
 
 export function Experience() {
     return (
             <div className="experience">
                 <Header />
+                <Item />
                 <h1>Work Experience & Education Timeline</h1>
                 <p>This is the experience profile page.</p>
                 <p>Here you can find information about the experience's skills and projects.</p>
---
+++ src/views/experience/item.jsx
@@ -0,0 +1,12 @@
+export function Item() {
+    return (
+        <div className="item">
+            <div className="content">
+                <p>Software Developer Watever</p>
+            </div>
+            <div className="time">
+                <p>2020 - 2023</p>
+            </div>
+        </div>
+    );
+}
